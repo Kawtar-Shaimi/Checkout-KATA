6b82b5103b99d42146924495b345f364ea14b8ae
Kawtar <Kawtar.shaimi8@gmail.com>
2026-02-13 15:04:28 +0100

    TC-3 : Rename prices to unitPrices for clarity

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,16 +1,16 @@
 module.exports = class Checkout {
     constructor() {
-        this.prices = new Map();
+        this.unitPrices = new Map();
         this.total = 0;
     }
 
     setUnitPrice(item, price) {
-        this.prices.set(item, price);
+        this.unitPrices.set(item, price);
     }
 
     scan(item) {
-        if (this.prices.has(item)) {
-            this.total += this.prices.get(item);
+        if (this.unitPrices.has(item)) {
+            this.total += this.unitPrices.get(item);
         }
     }
 
