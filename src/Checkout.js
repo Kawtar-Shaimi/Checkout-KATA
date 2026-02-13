d0f1151d14a64750b4d0c98083264bec64b5665c
Kawtar <Kawtar.shaimi8@gmail.com>
2026-02-13 16:09:11 +0100

    TC-11 : Implement markdown logic (GREEN)

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,11 +1,16 @@
 module.exports = class Checkout {
     constructor() {
         this.unitPrices = new Map();
+        this.markdowns = new Map();
         this.total = 0;
     }
 
     setUnitPrice(item, price) {
         this.unitPrices.set(item, price);
+    }
+
+    addMarkdown(item, amount) {
+        this.markdowns.set(item, amount);
     }
 
     scan(item, weight = 1) {
@@ -15,7 +20,11 @@ module.exports = class Checkout {
     }
 
     calculateItemPrice(item, weight) {
-        return this.unitPrices.get(item) * weight;
+        let price = this.unitPrices.get(item);
+        if (this.markdowns.has(item)) {
+            price -= this.markdowns.get(item);
+        }
+        return price * weight;
     }
 
     currentTotal() {
