aa66e0badc2779bca17eea8879825dbebc80b8c2
Kawtar <Kawtar.shaimi8@gmail.com>
2026-02-13 12:02:15 +0100

    TC-3 : Implement item scanning logic (Green)

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,3 +1,20 @@
 module.exports = class Checkout {
-    
+    constructor() {
+        this.prices = new Map();
+        this.total = 0;
+    }
+
+    setUnitPrice(item, price) {
+        this.prices.set(item, price);
+    }
+
+    scan(item) {
+        if (this.prices.has(item)) {
+            this.total += this.prices.get(item);
+        }
+    }
+
+    currentTotal() {
+        return this.total;
+    }
 }
